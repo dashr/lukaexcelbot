9b57aee0e9ba7ef082dc482693887fbe348e996b
jose d lopez <jose@devlabs.vc>
2018-02-10 23:32:37 -0300

    para monitoreo

--- web.js
+++ web.js
@@ -9,6 +9,11 @@ app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
+app.get('/heartbeat', function (req, res) {
+  res.send("ok");
+});
+
+
 var server = app.listen(process.env.PORT, "0.0.0.0", function () {
   var host = server.address().address;
   var port = server.address().port;
