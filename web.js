93732f3c7a706d3d0720ca033af7171d844e61a0
jose d lopez <jose@devlabs.vc>
2018-02-10 21:49:24 -0300

    servidor web, necessita cambios

---
+++ web.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var packageInfo = require('./package.json');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.json({ version: packageInfo.version });
+});
+
+var server = app.listen(process.env.PORT, "0.0.0.0", function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Servidor web inicio en http://%s:%s', host, port);
+});
+
+module.exports = function (bot) {
+  app.post('/' + bot.token, function (req, res) {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  });
+};
