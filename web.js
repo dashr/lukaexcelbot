a1a427a239f8fce0c6763811a617323a2b7c1509
jose d lopez <jose@devlabs.vc>
2018-02-17 15:58:48 -0300

    quickies, no optimizadas en librerias, pero ya

--- web.js
+++ web.js
@@ -3,6 +3,9 @@ var express = require('express');
 var helmet = require('helmet');
 var packageInfo = require('./package.json');
 var bodyParser = require('body-parser');
+var _ = require('lodash/core');
+var Redis = require('ioredis');
+var redis = new Redis(process.env.REDIS_URL);
 
 var app = express();
 app.use(helmet());
@@ -16,7 +19,39 @@ app.get('/heartbeat', function (req, res) {
   res.send("ok");
 });
 
-var server = app.listen(process.env.PORT, "0.0.0.0", function () {
+app.get('/libro_ventas', function (req, res) {
+  redis.zrevrange('ventas', 0, -1).then(function (result) {
+    var pipeline = redis.pipeline();
+    result.forEach(function(key) {
+      pipeline.hgetall(key);
+    });
+    pipeline.exec(function (err, results) {
+    var filtered = _.map(results, _.last);
+      res.json({
+        status: 'ok',
+        data: filtered
+      });
+    });
+  });
+});
+
+app.get('/libro_compras', function (req, res) {
+  redis.zrange('compras', 0, -1).then(function (result) {
+    var pipeline = redis.pipeline();
+    result.forEach(function(key) {
+      pipeline.hgetall(key);
+    });
+    pipeline.exec(function (err, results) {
+    var filtered = _.map(results, _.last);
+      res.json({
+        status: 'ok',
+        data: filtered
+      });
+    });
+  });
+});
+
+var server = app.listen(65483, "0.0.0.0", function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Servidor web inicio en http://%s:%s', host, port);
